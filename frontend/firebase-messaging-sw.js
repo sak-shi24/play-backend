ef358fec18f68fcf5564326f10dd0aa32ca1881c
sak-shi24 <154061504+sak-shi24@users.noreply.github.com>
2026-03-19 22:13:03 +0530

    Create firebase-messaging-sw.js

---
+++ frontend/firebase-messaging-sw.js
@@ -0,0 +1,13 @@
+importScripts('https://www.gstatic.com/firebasejs/12.11.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/12.11.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBBNE3vjnOMFIDWdFzm0NIsoaFsiBDsb6k",
+  authDomain: "play-and-earn-ba282.firebaseapp.com",
+  projectId: "play-and-earn-ba282",
+  storageBucket: "play-and-earn-ba282.firebasestorage.app",
+  messagingSenderId: "905951699525",
+  appId: "1:905951699525:web:f096537b769b3d7aa1392c"
+});
+
+const messaging = firebase.messaging();
