33793106c29a442d83c71275bc586e2448815c60
sak-shi24 <154061504+sak-shi24@users.noreply.github.com>
2026-03-24 09:52:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,6 +25,20 @@ app.use(express.urlencoded({ extended: true }));
 require("./config/db");
 
 /* =========================
+   FIREBASE ADMIN
+========================= */
+
+const admin = require("firebase-admin");
+
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+  })
+});
+
+/* =========================
    ROUTES
 ========================= */
 
